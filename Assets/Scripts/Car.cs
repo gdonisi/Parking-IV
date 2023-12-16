f081fa15cd8ae6fbd0543bac325f0b5d3b90de20
Francesco Maddaloni <52508617+frenkmadda@users.noreply.github.com>
2023-12-16 13:01:01 +0100

    Aggiunto respawn premendo E o uscendo dalla mappa

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -6,12 +6,16 @@ public class Car : MonoBehaviour
 {
     public float movingSpeed;
     public float rotationSpeed;
+    private Vector3 originalPos;
+    private Quaternion originalRot;
 
     // Start is called before the first frame update
     void Start()
     {
         movingSpeed = 20f;
         rotationSpeed = 85f;
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        originalRot = new Quaternion(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
     }
 
     // Update is called once per frame
@@ -33,11 +37,26 @@ public class Car : MonoBehaviour
                 rotation *= -1;
             }
         }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.position = originalPos;
+            transform.rotation = originalRot;
+        }
 
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
 
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Respawn"))
+        {
+            transform.position = originalPos;
+            transform.rotation = originalRot;
+        }
+            
     }
 }
